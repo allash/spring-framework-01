83fe65df8254c443f6fa1ca4631dd3cda158a234
allash <allashremoter@gmail.com>
2018-12-05 23:18:06 +0100

    small fixes

--- src/main/java/ru/otus/spring01/domain/Question.java
+++ src/main/java/ru/otus/spring01/domain/Question.java
@@ -5,6 +5,8 @@ public class Question {
     private String question;
     private String answer;
 
+    public Question() { }
+
     public Question(String question, String answer) {
         this.question = question;
         this.answer = answer;
